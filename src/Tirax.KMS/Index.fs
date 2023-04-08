0d388ebbb950240a567493c532fa7abb21f104ba
Ruxo Zheng <rz@just.sent.as>
2023-04-09 03:06:38 +0700

    initial commit

---
+++ src/Tirax.KMS/Index.fs
@@ -0,0 +1,38 @@
+namespace Tirax.KMS
+
+open Microsoft.AspNetCore.Mvc.Rendering
+open Fun.Blazor
+
+
+type Index() =
+    inherit FunBlazorComponent()
+
+    override _.Render() =
+#if DEBUG       
+        html.hotReloadComp (Tirax.KMS.App.app, "Tirax.KMS.App.app")
+#else
+        Tirax.KMS.App.app
+#endif
+
+    static member page ctx =
+        fragment {
+            doctype "html"
+            html' {
+                head {
+                    title { "Fun Blazor" }
+                    baseUrl "/"
+                    meta { charset "utf-8" }
+                    meta {
+                        name "viewport"
+                        content "width=device-width, initial-scale=1.0"
+                    }
+                }
+                body {
+                    rootComp<Index> ctx RenderMode.ServerPrerendered
+                    script { src "_framework/blazor.server.js" }
+#if DEBUG
+                    html.hotReloadJSInterop
+#endif
+                }
+            }
+        }
