b5072ede3e74208d7311bd0bed08fa029ac61cf7
Ruxo Zheng <rz@just.sent.as>
2023-07-26 06:56:46 +0700

    KMS-2 Finish update uptime every hour

--- src/Tirax.KMS.Core/Akka/ScheduledWorker.cs
+++ src/Tirax.KMS.Core/Akka/ScheduledWorker.cs
@@ -1,10 +1,28 @@
 ﻿using Akka.Actor;
+using LanguageExt.UnitsOfMeasure;
+using Tirax.KMS.Server;
 
 namespace Tirax.KMS.Akka;
 
-public sealed class ScheduledWorker : ReceiveActor, IActorFacade
+public sealed class ScheduledWorker : ReceiveActor, IWithTimers, IActorFacade
 {
-    public ScheduledWorker() {
-        
+    /// <summary>
+    /// Internal use!
+    /// </summary>
+    public ITimerScheduler Timers { get; set; } = null!;
+    
+    public ScheduledWorker(IKmsServer kmsServer) {
+        var now = DateTime.UtcNow;
+        var nextHour = now + 1.Hours();
+        var nextStartHour = new DateTime(nextHour.Year, nextHour.Month, nextHour.Day, nextHour.Hour, 0, 0, DateTimeKind.Utc);
+        var dueTime = nextStartHour - now;
+        Timers.StartPeriodicTimer("uptime", Uptime.Default, dueTime, 1.Hours());
+
+        ReceiveAsync<Uptime>(async _ => await kmsServer.RecordUpTime());
+    }
+
+    sealed record Uptime
+    {
+        public static readonly Uptime Default = new();
     }
 }
