6811452e69e3960336e56d4bab61e210e4687b6b
Ruxo Zheng <rz@just.sent.as>
2023-07-26 06:14:39 +0700

    KMS-2 Setup Akka system

---
+++ src/Tirax.KMS.App/Services/Akka/AkkaService.cs
@@ -0,0 +1,30 @@
+﻿using Akka.Actor;
+using Akka.DependencyInjection;
+using Tirax.KMS.Akka;
+
+namespace Tirax.KMS.App.Services.Akka;
+
+public sealed class AkkaService : IHostedService, IActorFacade
+{
+    readonly Lazy<ActorSystem> system;
+    
+    public AkkaService(IServiceProvider serviceProvider, IHostApplicationLifetime appLifetime, IConfiguration configuration) {
+        system = new(() => {
+            var bootstrap = BootstrapSetup.Create();
+            var diSetup = DependencyResolverSetup.Create(serviceProvider);
+            var systemOptions = bootstrap.And(diSetup);
+            var sys = ActorSystem.Create("TiraxKMS", systemOptions);
+            sys.WhenTerminated.ContinueWith(_ => appLifetime.StopApplication());
+            return sys;
+        });
+    }
+    
+    public Task StartAsync(CancellationToken cancellationToken) {
+        var workerProps = DependencyResolver.For(system.Value).Props<ScheduledWorker>();
+        system.Value.ActorOf(workerProps, "scheduled-worker");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => 
+        CoordinatedShutdown.Get(system.Value).Run(CoordinatedShutdown.ClrExitReason.Instance);
+}
---
+++ src/Tirax.KMS.Core/Akka/ScheduledWorker.cs
@@ -0,0 +1,10 @@
+﻿using Akka.Actor;
+
+namespace Tirax.KMS.Akka;
+
+public sealed class ScheduledWorker : ReceiveActor, IActorFacade
+{
+    public ScheduledWorker() {
+        
+    }
+}
