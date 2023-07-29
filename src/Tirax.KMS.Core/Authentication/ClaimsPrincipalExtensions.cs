5984a02800b73a953189ea8d40f795cc81f6c144
Ruxo Zheng <rz@just.sent.as>
2023-07-29 10:30:09 +0700

    KMS-7 Show user name

---
+++ src/Tirax.KMS.Core/Authentication/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace Tirax.KMS.Authentication;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static string UserName(this ClaimsPrincipal principal) =>
+        principal.FindFirst("name")?.Value ?? "(guest?)";
+}
